bf07a605f29f4800091fabe154dfcc69f9d8ce88
Skripnik <bbssvv06@gmail.com>
2017-04-25 23:43:26 +0300

    init

---
+++ SActiveQuery.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace frontend\components\scalarRelation;
+
+use yii\base\Exception;
+use yii\db\ActiveQuery;
+use yii\helpers\ArrayHelper;
+
+/**
+ * Класс ActiveQuery для получения скалярных связей
+ * После отработки self::populate() происходит заполнение SActiveRecord данными с результатами связей withScalar(), классов которые наследуются от SActiveRecord
+ * 
+ * @package frontend\components\scalarRelation
+ */
+class SActiveQuery extends ActiveQuery
+{
+
+  /**
+   * Массив связей с которым работает данный объект query
+   * @see yii\db\ActiveQueryTrait::with
+   * @var array
+   */
+  private $withScalarData;
+
+  /**
+   * Атрибуты модели по которым будет происходить групировка и идентификация модели
+   * @var array
+   */
+  private $groupAttributes;
+
+  /**
+   * Данные моделей групирируемых аттрибутов
+   * @var
+   */
+  private $groupAttributesModelsData;
+
+  /**
+   * Метод аналог обычного with для скалярных связей
+   * @see yii\db\ActiveQueryTrait::with()
+   * @param array $data массив связей
+   */
+  public function withScalar(array $data)
+  {
+    $this->withScalarData = $data;
+    return $this;
+  }
+
+  /**
+   * Установка аттрибутов для групировки для скалярных связей
+   * @param array $attributes массив аттрибутов для группировки
+   * @throws Exception
+   */
+  public function setGroupAttributes(array $attributes)
+  {
+    if (empty($attributes)) {
+      throw new Exception('Group attributes for relation not found');
+    }
+    $this->groupAttributes = $attributes;
+  }
+
+  /**
+   * Переопределение метода который возвращает данные после выполнения запроса
+   * @param array $rows
+   */
+  public function populate($rows)
+  {
+    $models = parent::populate($rows);
+    if (!empty($models) && $models[0] instanceof SActiveRecord) {
+      $this->scalarRelation($models);
+    }
+
+    return $models;
+  }
+
+  /**
+   * Логика получения и присваивания данных скалярной связи
+   * 
+   * @param array $models результирующий набор моделей
+   * @return void
+   * @throws Exception
+   */
+  private function scalarRelation($models)
+  {
+    if ($this->withScalarData != null && $models != null) {
+      $model = $models[0];
+
+      foreach ($this->withScalarData as $relationName) {
+        $relationsQuery = $this->getScalarRelationQuery($model, $relationName);
+        $groupAttributes = $relationsQuery->getGroupAttributes();
+        $select = $groupAttributes;
+        $select[$relationName] = array_shift($relationsQuery->select);
+        $relationsQuery->select($select);
+        if ($relationsQuery->groupBy != null) {
+          $groupAttributes = array_merge($relationsQuery->groupBy, $groupAttributes);
+        }
+        $relationsQuery->groupBy($groupAttributes);
+
+        $this->setRelationScalarDataForModel($models, $relationsQuery, $relationName);
+      }
+    }
+  }
+
+  /**
+   * Получение SActiveQuery для скалярной связи
+   * 
+   * @param SActiveRecord $model интстенс модели с набором данных
+   * @param string $relationName название связи (геттера) установленной как метод в моделе унаследованной от SActiveRecord
+   * 
+   * @return SActiveQuery объект запроса для нахождение значения по параметру, который группируется
+   * 
+   * @throws Exception
+   */
+  private function getScalarRelationQuery(SActiveRecord $model, $relationName)
+  {
+    $methodName = $relationName;
+    if (!method_exists($model, $methodName)) {
+      $modelClass = get_class($model);
+      throw new Exception("Method {$methodName} for relation {$relationName} not found in {$modelClass}");
+    }
+    $query = call_user_func([$model, $methodName]);
+    if (!$query instanceof SActiveQuery) {
+      throw new Exception('Relation not instanceof class SActiveQuery');
+    }
+    if ($query->select == null || count($query->select) > 1) {
+      throw new Exception('Set only one select in you relation SActiveQuery');
+    }
+
+    return $query;
+  }
+
+  /**
+   * Установка скалярных данных для каждой модели результата моиска
+   * @param array $models результирующий набор моделей
+   * @param SActiveQuery $relationsQuery объект запроса для нахождения значения по параметру, который группируется
+   * @param string $relationName название связи
+   */
+  private function setRelationScalarDataForModel($models, $relationsQuery, $relationName)
+  {
+    foreach ($relationsQuery->getGroupAttributes() as $attr) {
+      if (!isset($this->groupAttributesModelsData[$attr])) {
+        $this->groupAttributesModelsData[$attr] = ArrayHelper::getColumn($models, preg_replace('/^.+\./', '', $attr));
+      }
+      if (!empty($this->groupAttributesModelsData[$attr])) {
+        $relationsQuery->andWhere([$attr => array_unique($this->groupAttributesModelsData[$attr])]);
+      }
+    }
+
+    $data = $relationsQuery->asArray()->all();
+
+    foreach ($models as $model) {
+      $find = !empty($data);
+      foreach ($data as $row) {
+        $find = true;
+        foreach ($row as $attribute => $val) {
+          if (isset($model->{$attribute}) && $model->{$attribute} != $val) {
+            $find = false;
+            break;
+          }
+        }
+        if ($find)
+          break;
+      }
+      if ($find) {
+        $relationValue = $row[$relationName];
+        $model->setScalarRelation($relationName, $relationValue);
+      } else {
+        $model->setScalarRelation($relationName, 0);
+      }
+    }
+  }
+
+  /**
+   * Получение аттрибутов групировки для скалярных связей вида tableName.fieldName
+   * @return array аттрибуты для группировки вида ['filial_user.fk_user_id', 'filial_user.fk_user_id2']
+   */
+  private function getGroupAttributes()
+  {
+    $res = [];
+    $modelClass = $this->modelClass;
+    $tableName = $modelClass::tableName();
+    foreach ($this->groupAttributes as $attr) {
+      $res[] = "{$tableName}.{$attr}";
+    }
+    return $res;
+  }
+
+}
---
+++ SActiveRecord.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace frontend\components\scalarRelation;
+
+use Yii;
+use yii\base\Exception;
+use yii\db\ActiveRecord;
+
+/**
+ * Переопределение ActiveRecord для скалярных связей
+ * Скалярной связью будем называть количество сгруппированное по некоему параметру.
+ * Например:
+ * Есть список пользователей. У каждого пользователя есть количество активных заказов и отмененных. Нам нужно в списке пользователей вывести активные и отмененные заказы.
+ * Использование:
+ * В моделе, унаследованной от данного класса, прописываем связь, где задаем необходимые условия и поля для группировки (@see self::hasScalar()). Например:
+ * <pre>
+ * public function getActiveBuyerCount()
+  {
+    return $this->hasScalar(__FUNCTION__, ['fk_user_id', 'fksm_filial_id'])
+        ->select(['count(*)'])
+        ->innerJoin('buyer', 'filial_user.fk_user_id = buyer.rieltor_user_id')
+        ->where([
+            'buyer.archive_flag' => Buyer::ACTIVE_FLAG
+        ]);
+  }
+ * </pre>
+ * 
+ * @package frontend\components\scalarRelation
+ */
+class SActiveRecord extends ActiveRecord
+{
+
+  /**
+   * Массив данных в который будут складываться искомые скалярные величины
+   * @var array
+   */
+  private $scalarValues = [];
+
+  /**
+   * Переопределение метода для возвращения собственного ActiveQuery
+   * 
+   * @see http://www.yiiframework.com/doc-2.0/yii-db-activerecord.html#find()-detail
+   * @overriding
+   * @return object
+   * @throws \yii\base\InvalidConfigException
+   */
+  public static function find()
+  {
+    return Yii::createObject(SActiveQuery::className(), [get_called_class()]);
+  }
+
+  /**
+   * Объявление связи, установка аттрибутов для группировки, возврат объекта SActiveQuery для просчета необходимых параметров
+   * Если аттрибуты для группировки пустые - группировка производится по первичному ключу
+   *
+   * Для вызова просчитанного результата используется $this->getScalarRelation("getMethodName"), где getMethodName соответствующий геттер в моделе наследнике
+   *
+   * @param string $method название метода от которого вызывается hasScalar()
+   * @param array $groupAttributes аттрибуты модели по которым производится группировка
+   * @return frontend\components\scalarRelation\SActiveQuery
+   * @throws Exception
+   */
+  public function hasScalar($method, $groupAttributes = null)
+  {
+    if ($groupAttributes == null) {
+      $groupAttributes = $this->primaryKey();
+    }
+    if (!method_exists($this, $method)) {
+      throw new Exception("Add method {$method} in " . self::className());
+    }
+    $query = self::find();
+    $query->setGroupAttributes($groupAttributes);
+    return $query;
+  }
+
+  /**
+   * Получение значения скалярной связи
+   * 
+   * @param string $methodName название связи (геттера) установленной как метод в моделе наследнике
+   * @return string | null | false @see yii\db\Query::scalar()
+   */
+  public function getScalarRelation($methodName)
+  {
+    if (!isset($this->scalarValues[$methodName])) {
+      $query = call_user_func([$this, $methodName]);
+      $primaryKeys = $this->primaryKey();
+      $condition = [];
+      foreach ($this->getAttributes($primaryKeys) as $attr => $value) {
+        $condition[$this->tableName().'.'.$attr] = $value;
+      }
+      $query->andWhere($condition);
+      if (!$query instanceof SActiveQuery) {
+        throw new Exception("Method {$methodName} must be instance of SActiveQuery");
+      }
+      $this->scalarValues[$methodName] = $query->scalar();
+    }
+    return $this->scalarValues[$methodName];
+  }
+
+  /**
+   * Присваивание результата выполнения скалярной величины
+   * Вызывается в frontend\components\scalarRelation\SActiveQuery::setRelationScalarDataForModel()
+   * 
+   * @param $methodName название связи (геттера) установленной как метод в моделе наследнике
+   * @param null
+   */
+  public function setScalarRelation($methodName, $value)
+  {
+    $this->scalarValues[$methodName] = $value;
+  }
+
+}
